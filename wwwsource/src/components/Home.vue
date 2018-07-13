ee26815649a7d87b4031a34040423de61749a798
WestonDockstader <dockstaderweston@gmail.com>
2018-07-13 01:35:58 -0600

    added login and register modals

--- wwwsource/src/components/Home.vue
+++ wwwsource/src/components/Home.vue
@@ -1,20 +1,126 @@
 <template>
-  <div class="home">
+  <div class="home container-fluid">
+    <nav class="navbar navbar-dark bg-dark">
+      <h2 class="navbar-heading">KEEPR</h2>
+      <div>
+        <div v-if="!user.username">
+          <button class="btn btn-outline-info" data-toggle="modal" data-target="#registerModal">Register</button>
+          <button class="btn btn-outline-info" data-toggle="modal" data-target="#loginModal">Login</button>
+        </div>
+        <div v-if="user.username">
+          <h3>Hello {{user.username}}</h3>
+          <button class="btn btn-outline-info">Logout</button>
+        </div>
+      </div>
+    </nav>
+    <!-- This is the register modal -->
+    <div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalCenterTitle">Register</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form v-on:submit.prevent="userLogin">
+              <div class="form-group">
+                <input type="text" name="username" v-model="register.username" class="form-control" id="formGroupExampleInput" placeholder="Username"
+                  required>
+              </div>
+              <div class="form-group">
+                <input type="text" name="email" v-model="register.email" class="form-control" id="formGroupExampleInput" placeholder="email" autocomplete="email">
+              </div>
+              <div class="form-group">
+                <input type="text" name="password" v-model="register.password" class="form-control" id="formGroupExampleInput" placeholder="Password">
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submits" class="btn btn-primary" @click="userRegister">Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- This is the login modal -->
+    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalCenterTitle">Login</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form v-on:submit.prevent="userLogin">
+                <div class="form-group">
+                  <input type="text" name="username" v-model="login.username" class="form-control" id="formGroupExampleInput" placeholder="Username"
+                    required>
+                </div>
+                <div class="form-group">
+                  <input type="text" name="password" v-model="login.password" class="form-control" id="formGroupExampleInput" placeholder="Password">
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary" @click="userLogin">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    <div class="row">
     
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-  data () {
-    return {
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        register: {
+          username: "",
+          email: "",
+          password: ""
+        },
+        login: {
+          username: "",
+          password: ""
+        }
+      }
+    },
+    computed: {
+      user() {
+        return this.$store.state.user
+      }
+    },
+    methods: {
+      userLogin() {
+        this.$store.dispatch('login', this.login)
+      },
+      userRegister() {
+        console.log("register method")
+        this.$store.dispatch('register', this.register)
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .navbar {
+    background-color: darkslategray;
+    color: rgb(87, 236, 241);
+    height: 100%;
+  }
+
+  .navbar-heading {
+    padding: 3px;
+  }
 
 </style>
--- wwwsource/src/store/index.js
+++ wwwsource/src/store/index.js
@@ -13,8 +13,32 @@ var api=axios.create({
   withCredentials: true
 })
 
+var auth = axios.create({
+  baseURL: baseUrl + 'account/',
+  timeout: 3000,
+  withCredentials: true
+})
+
 export default new vuex.Store({
-  state:{},
-  mutations:{},
-  actions:{}
+  state:{
+    user: {}
+  },
+  mutations:{
+    setUser(state,payload){
+      state.user=payload
+    }
+  },
+  actions:{
+    register({commit}, payload){
+      console.log("register action",payload)
+      auth.post('register',payload)
+        .then(res=>{
+          console.log(res)
+          commit('setUser',res.data)
+        })
+        .catch(err=>{
+          console.log(err)
+        })
+    }
+  }
 })
